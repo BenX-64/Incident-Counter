499756341419202ea9388fab0aefd2f0e7e83419
Ben <benjx32@gmail.com>
2021-12-13 16:27:50 -0800

    pls work php

--- website_stuff/create_counter.php
+++ website_stuff/create_counter.php
@@ -1,6 +1,7 @@
 <?php
+    error_reporting(0);
     $incname = $_POST['incname'];
 
     //Database Connection Shenanigans
-    
+    $con = mysqli_connect("localhost","my_user","my_password","incidentcounter");
 ?>
