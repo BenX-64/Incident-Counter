7f0f991c02a3cb9543e4d3f8630b69ad79fe0720
Ben <benjx32@gmail.com>
2021-12-13 20:20:39 -0800

    php stuff works but not complete

--- website_stuff/create_counter.php
+++ website_stuff/create_counter.php
@@ -2,6 +2,25 @@
     error_reporting(0);
     $incname = $_POST['incname'];
 
+    $db_host = "localhost";
+    $db_user = "root";
+    $db_pwd = "";
+    $db_name = "incidentcounter";
     //Database Connection Shenanigans
-    $con = mysqli_connect("localhost","my_user","my_password","incidentcounter");
+    $conn = new mysqli($db_host,$db_user,$db_pwd,$db_name);
+
+// Check connection
+    if ($conn -> connect_error) {
+        echo "Failed to connect to MySQL: " . $conn -> connect_error;
+    exit();
+    }
+    $date = date("y-m-d");
+    $sql = "INSERT INTO `incidents`(`tag`, `incname`, `inctotal`, `inclaston`) VALUES ('[value-1]','$incname','[value-3]','$date')";
+    if($conn->query($sql) == TRUE){
+        echo "it works!";
+    }else{
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    $conn->close();
+
 ?>
