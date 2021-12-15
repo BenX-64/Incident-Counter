cfd25853f0882a579fb12dcfe29ed685f4ddf37e
Ben <benjx32@gmail.com>
2021-12-15 15:17:16 -0800

    no js

--- website_stuff/incidentpage.php
+++ website_stuff/incidentpage.php
@@ -21,27 +21,30 @@
             $conn = new mysqli($db_host,$db_user,$db_pwd,$db_name);
             $sql = "SELECT tag, incname,inctotal,inclaston FROM incidents WHERE tag = $id";
             $result = $conn->query($sql);
-            $row = $result->fetch_array();
-
-            echo "<h1>";
-            echo $row["incname"];
-            echo "</h1>";
-            echo "<div class='counter'>";
-            echo $row["inctotal"];
-            echo "</div>";
-            echo "<div>";
-            echo "Total Incidents";
-            echo "<br>";
-            echo "Last Incident On: ";
-            echo $row["inclaston"];
-            echo "<br>";
-            echo "Tag: ".$row["tag"];
-            echo "</div>";
-
-            echo "<p><form action = '../server_stuff/increment_counter.php?counter=".$id,"' method = 'post'>
-                <input type='submit' value = 'Increment'>
-            </form>
-            </p>";
+            if($result->num_rows == 0){
+                echo "<h1>Error 404: Counter not found.</h1>";
+            }else{
+                $row = $result->fetch_array();
+                echo "<h1>";
+                echo $row["incname"];
+                echo "</h1>";
+                echo "<div class='counter'>";
+                echo $row["inctotal"];
+                echo "</div>";
+                echo "<div>";
+                echo "Total Incidents";
+                echo "<br>";
+                echo "Last Incident On: ";
+                echo $row["inclaston"];
+                echo "<br>";
+                echo "Tag: ".$row["tag"];
+                echo "</div>";
+    
+                echo "<p><form action = '../server_stuff/increment_counter.php?counter=".$id,"' method = 'post'>
+                    <input type='submit' value = 'Increment'>
+                </form>
+                </p>";
+            }
         ?>
     </body>
 
